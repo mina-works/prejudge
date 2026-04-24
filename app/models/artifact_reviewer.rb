2afe325e6d59e954f362d9253930f135c997f599
IkedaMinako <i.minako@people.co.jp>
2026-04-24 15:03:21 +0900

    add status enum and validations to artifactReviewer model

--- app/models/artifact_reviewer.rb
+++ app/models/artifact_reviewer.rb
@@ -1,4 +1,11 @@
 class ArtifactReviewer < ApplicationRecord
   belongs_to :artifact
   belongs_to :user
+
+  enum role: {
+    reviewer: 0,
+    approver: 1
+  }
+
+  validates :role, presence: true
 end
