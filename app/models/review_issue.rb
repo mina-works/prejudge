9df5917636e6b31bccf932c2e74a93a62f0c9aa7
IkedaMinako <i.minako@people.co.jp>
2026-04-27 17:27:32 +0900

    add review issue validations

--- app/models/review_issue.rb
+++ app/models/review_issue.rb
@@ -1,3 +1,11 @@
 class ReviewIssue < ApplicationRecord
   belongs_to :review
+
+  enum issue_type: {
+    wrong_atmosphere: 0,
+    wrong_target: 1,
+    tone_mismatch: 2,
+    too_much_or_little_info: 3,
+    direction_issue: 4
+  }
 end
