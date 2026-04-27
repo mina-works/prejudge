b53150c5b5e9023e9464248c4e1109133d83eddc
IkedaMinako <i.minako@people.co.jp>
2026-04-27 15:58:10 +0900

    add review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review < ApplicationRecord
   belongs_to :artifact
   belongs_to :user
+
+  enum result: {
+    ok: 0,
+    uneasy: 1,
+    ng: 2
+  }
+
+  before_validation :set_round
+  validate :prevent_edit_past_review, on: :update
+
+  private
+
+  def set_round
+    self.round ||= artifact&.current_round
+  end
+
+  def prevent_edit_past_review
+    if round < artifact.current_round
+      errors.add(:base, "過去ラウンドのレビューは編集できません")
+    end
+  end
 end
