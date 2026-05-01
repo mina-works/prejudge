1d6a2c0beeee7fc892fa7ac8c3c888e50749085f
IkedaMinako <i.minako@people.co.jp>
2026-05-01 13:56:03 +0900

    add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # associations
+  has_many :artifacts, foreign_key: :creator_id
+  has_many :artifact_reviewers
+  has_many :reviews, dependent: :nullify
+
+  # validations
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
