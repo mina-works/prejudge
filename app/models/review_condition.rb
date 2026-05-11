40ba550165296feaa2a43240d511b507aaefbd98
IkedaMinako <i.minako@people.co.jp>
2026-05-11 17:41:59 +0900

    fix review condition enum

--- app/models/review_condition.rb
+++ app/models/review_condition.rb
@@ -11,13 +11,13 @@ class ReviewCondition < ApplicationRecord
     internal_members: 6
   }
 
-  enum target: {
-    students: 0,
-    job_seekers: 1,
-    local_government_staff: 2,
-    companies: 3,
-    clients: 4,
-    residents: 5,
-    internal_members: 6
+  enum tone: {
+    serious: 0,
+    friendly: 1,
+    warm: 2,
+    professional: 3,
+    modern: 4,
+    casual: 5,
+    luxurious: 6
   }
 end
