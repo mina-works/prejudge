4af67f16990a65386793d43824f5f0eea46d5a79
IkedaMinako <i.minako@people.co.jp>
2026-05-11 15:55:36 +0900

    add artifact reviewe condition enum

--- app/models/review_condition.rb
+++ app/models/review_condition.rb
@@ -1,3 +1,23 @@
 class ReviewCondition < ApplicationRecord
   belongs_to :artifact
+
+  enum target: {
+    students: 0,
+    job_seekers: 1,
+    local_government_staff: 2,
+    companies: 3,
+    clients: 4,
+    residents: 5,
+    internal_members: 6
+  }
+
+  enum target: {
+    students: 0,
+    job_seekers: 1,
+    local_government_staff: 2,
+    companies: 3,
+    clients: 4,
+    residents: 5,
+    internal_members: 6
+  }
 end
