5f95178d41a736831580018b3b79f03d265bc62e
IkedaMinako <i.minako@people.co.jp>
2026-06-04 17:00:20 +0900

    add translations for review issues

--- app/models/review_issue.rb
+++ app/models/review_issue.rb
@@ -12,6 +12,13 @@ class ReviewIssue < ApplicationRecord
   # reviewがokの場合はissueを持てない
   validate :review_must_not_be_ok
 
+  # issue_type(enum)を日本語ラベルに変換する
+  def self.issue_type_label(issue_type)
+    I18n.t(
+      "enums.review_issue.issue_type.#{issue_type}"
+    )
+  end
+
   private
 
   def review_must_not_be_ok
