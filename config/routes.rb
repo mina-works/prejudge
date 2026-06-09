088c4d2af67cf98f29eb89e7b261c4af933670d2
Mina <134347875+mina-works@users.noreply.github.com>
2026-06-09 23:16:18 +0900

    add review index

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   
-  resources :reviews, only: [:new, :create, :show]
+  resources :reviews, only: [:index, :new, :create, :show]
 end
